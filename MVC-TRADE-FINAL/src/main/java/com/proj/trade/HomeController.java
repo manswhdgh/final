19a36ab085eefbc7eb4e8d4273f62ae04807df8d
kornjjang <kornjjang@gmail.com>
2019-08-21 16:25:01 +0900

    작업내용 : 정재덕 뷰 마감

--- MVC-TRADE-FINAL/src/main/java/com/proj/trade/HomeController.java
+++ MVC-TRADE-FINAL/src/main/java/com/proj/trade/HomeController.java
@@ -11,12 +11,17 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-
+/**
+ * Handles requests for the application home page.
+ */
 @Controller
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
@@ -28,228 +33,7 @@ public class HomeController {
 		
 		model.addAttribute("serverTime", formattedDate );
 		
-		return "admin";
-	}
-	@RequestMapping(value = "/test", method = RequestMethod.GET)
-	public String test(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "admin1";
-	}
-	@RequestMapping(value = "/admPro", method = RequestMethod.GET)
-	public String admPro(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "admPromo";
-	}
-	@RequestMapping(value = "/addBanList", method = RequestMethod.GET)
-	public String addBanList(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "addBanList";
-	}
-	@RequestMapping(value = "/admTradeListStatus", method = RequestMethod.GET)
-	public String admTradeListStatus(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "admTradeListStatus";
-	}
-	@RequestMapping(value = "/admRecord", method = RequestMethod.GET)
-	public String admRecord(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "admRecord";
-	}
-	@RequestMapping(value = "/newbieList", method = RequestMethod.GET)
-	public String newbieList(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "newbieList";
-	}
-	@RequestMapping(value = "/notiBoard", method = RequestMethod.GET)
-	public String notiBoard(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "notiBoard";
-	}
-	@RequestMapping(value = "/inquiry", method = RequestMethod.GET)
-	public String inquiry(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "inquiry";
-	}
-	@RequestMapping(value = "/livechatList", method = RequestMethod.GET)
-	public String livechatList(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "livechatList";
-	}
-	@RequestMapping(value = "/boardReport", method = RequestMethod.GET)
-	public String boardReport(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "boardReport";
-	}
-	@RequestMapping(value = "/viewQna", method = RequestMethod.GET)
-	public String viewQna(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "viewQna";
-	}
-	@RequestMapping(value = "/adminPromoConfirmAjax", method = RequestMethod.GET)
-	public String adminPromoConfirmAjax(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "adminPromoConfirmAjax";
-	}
-	@RequestMapping(value = "/admTradeListStatusAjax", method = RequestMethod.GET)
-	public String admTradeListStatusAjax(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "admTradeListStatusAjax";
-	}
-	@RequestMapping(value = "/notiBoardAjax", method = RequestMethod.GET)
-	public String notiBoardAjax(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "notiBoardAjax";
-	}
-	@RequestMapping(value = "/inquiryAjax", method = RequestMethod.GET)
-	public String inquiryAjax(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "inquiryAjax";
-	}
-	@RequestMapping(value = "/reportAjax", method = RequestMethod.GET)
-	public String reportAjax(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "reportAjax";
-	}
-	@RequestMapping(value = "/viewQnaAjax", method = RequestMethod.GET)
-	public String viewQnaAjax(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "viewQnaAjax";
+		return "buy/buyBefore";
 	}
 	
 }
---
+++ MVC-TRADE-FINAL/src/main/webapp/WEB-INF/views/auction/auctionBoard.jsp
@@ -0,0 +1,172 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<style>
+	.uppermenu{
+		width:100%;
+		height:200px;
+		
+		display:inline-flex;
+		
+	}
+	 .middlemenu{
+	 	width:1500px;
+	 	height:1000px;
+	 	border:solid 1px black;
+	 	display:inline-felx;
+	 	margin-top:20px;
+		display:inline-flex;
+	 }
+	 .firstdiv{
+		width:1300px;
+		height:200px;
+		margin-left:10%;
+		display:inline-flex;
+		
+	}
+	.seconddiv{
+		width:300px;
+		height:200px;
+	}
+	.gamestyle{
+		font-style:맑은고딕;
+		font-size:35px;
+		margin-left:40px;
+	
+	}
+	.gamesize{
+		width:200px;
+		height:30px;
+		margin-top:20px;
+	}
+	.serverstyle{
+		font-style:맑은고딕;
+		font-size:35px;
+		margin-left:10%;
+	}
+	.serversize{
+		width:200px;
+		height:30px;
+		margin-top:20px;
+	}
+	.itemgroupstyle{
+		font-style:맑은고딕;
+		font-size:35px;
+		margin-left:10%;
+	}
+	.itemgroupsize{
+		width:200px;
+		height:30px;
+		margin-top:20px;
+	}
+	.auctiongroupstyle{
+		font-style:맑은고딕;
+		font-size:35px;
+		margin-left:10%;
+	}
+	.auctiongroupsize{
+		width:200px;
+		height:30px;
+		margin-top:20px;
+	}
+	.button1{
+		width:1000px;
+		height:40px;
+		position:absolute;
+		margin-top:150px;
+		margin-left:8%;
+	} 
+	.button2{
+		width:100px;
+		height:100px;
+		margin-top:51px;
+	}
+	.table{
+		border-padding:15px;
+		border-spacing:25px;
+	}
+	.th1{
+		width:100px;
+		height:30px;
+		border:solid 1px black;
+		font-style:"맑은고딕";
+		
+	}
+	.th2{
+		width:550px;
+		height:30px;
+		border:solid 1px black;
+		font-style:"맑은고딕";
+		
+	}
+	.th3{
+		width:150px;
+		height:30px;
+		border:solid 1px black;
+		font-style:"맑은고딕";
+	}
+	
+</style>
+<title>Insert title here</title>
+</head>
+<body>
+	 <div class="uppermenu">
+	  <div class="firstdiv">
+		<p class="gamestyle">게임
+		<select class="gamesize">
+			<option>::선택::</option>
+			<option selected="selected">게임1</option>
+			<option>게임2</option>
+			<option>게임3</option>
+		</select>
+		</p>
+		<br>
+		<button class="button1" type="button">경매등록</button>
+		<p class="serverstyle">서버
+		<select class="serversize">
+		<option>::선택::</option>
+		<option selected="selected">서버1</option>
+		<option>서버2</option>
+		<option>서버3</option>
+		</select>
+		</p>
+		<p class="itemgroupstyle">물품유형
+		<select class="itemgroupsize">
+		<option>::선택::</option>
+		<option selected="selected">무기</option>
+		<option>방어구</option>
+		<option>게임머니</option>
+		</select>
+		</p>
+		<p class="auctiongroupstyle">경매유형
+		<select class="auctiongroupsize">
+		<option>::선택::</option>
+		<option selected="selected">경매</option>
+		<option>역경매</option>
+		</select>
+		</p>
+		</div>
+		<div class="seconddiv">
+		<center>
+		<button class="button2" type="button">검색</button>
+		</center>
+		</div>
+	</div>
+ 	<center>	
+ 	<div class="middlemenu">
+ 		<table class="table">
+ 			<th class="th1">글번호</th>
+ 			<th class="th2">물품제목</th>
+ 			<th class="th3">이름</th>	 
+ 			<th class="th3">상한가</th>
+ 			<th class="th3">입찰시간</th>
+ 			<th class="th3">등록일자</th>
+ 		</table>
+ 	</div>
+ 	</center>
+</body>
+</html>
